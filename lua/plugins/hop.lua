54f370f59fdb8250c77c72bcf263fdd70374b735
Mikhail Kyzyma <mkyzyma@gmail.com>
2024-07-08 16:04:07 +0500

    Initial commit

---
+++ lua/plugins/hop.lua
@@ -0,0 +1,9 @@
+return {
+  'smoka7/hop.nvim',
+  opts = function()
+    require("hop").setup();
+    vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+    vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+  end
+}
+
